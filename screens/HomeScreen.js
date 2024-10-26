dd7c043f83bbd3d1d7e30f1b1cfba45dd651dd00
tiffany <tytywyz@gmail.com>
2024-10-26 15:49:41 -0400

    working create new workout page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,16 @@
 // screens/HomeScreen.js
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, TextInput, TouchableOpacity, Switch, StyleSheet, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebaseConfig';
 import { getFirestore, collection, addDoc, Timestamp } from '@firebase/firestore';
-import { timeStamp } from 'console';
-import { TextInput } from 'react-native-gesture-handler';
 
 const db = getFirestore();
 
+const blue = '#5acfe9';
+
+//this is acutally the create new workout screen i can't rename it tho it goes cray cray
 const HomeScreen = () => {
 
   const [workoutName, setWorkoutName] = useState('');
@@ -32,14 +34,13 @@ const HomeScreen = () => {
           numReps: parseInt(numReps),
           usesWeight: usesWeight,
           numPounds: usesWeight ? parseInt(numPounds) : 0,
-          timeStamp: new Date(),
+          timeStamp: Timestamp.now(),
         });
         setWorkoutName(''); //clears input after adding workout
         setNumReps('');
         setUsesWeight(false);
         setNumPounds('');
         alert('Workout recorded!');
-
       } catch (error) {
         console.error('Error adding workout: ', error);
       }
@@ -49,35 +50,108 @@ const HomeScreen = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome!</Text>
-      <Text style={styles.email}>{auth.currentUser?.email}</Text>
-      
-      <TextInput
-        style={styles.input}
-        placeholder="Exercise Name"
-        value={workoutName}
-        onChangeText={setWorkoutName}
-      />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text 
+        style={styles.title}>Enter your exercise information:
+        </Text>
+        
+        {/*
+        <TextInput
+          style={styles.inputBox }
+          placeholder="Exercise Name"
+          value={workoutName}
+          onChangeText={setWorkoutName}
+        />
+        */}
 
-      <TextInput
-        style={styles.input}
-        placeholder="Reps"
-        value={numReps}
-        onChangeText={setNumReps}
-        keyboardType="numeric"
-      />
+        <View>
+          <Picker
+            selectedValue={workoutName}
+            onValueChange={(itemValue) => setWorkoutName(itemValue)}
+          >
+            <Picker.Item label="Bench Press" value="Bench Press" />
+            <Picker.Item label="Push-Ups" value="Push-Ups" />
+            <Picker.Item label="Dumbbell Chest Fly" value="Dumbbell Chest Fly" />
+            <Picker.Item label="Pull-Ups" value="Pull-Ups" />
+            <Picker.Item label="Rows" value="Rows" />
+            <Picker.Item label="Lat Pull-Downs" value="Lat Pull-Downs" />
+            <Picker.Item label="Shoulder Press" value="Shoulder Press" />
+            <Picker.Item label="Lateral Raises" value="Lateral Raises" />
+            <Picker.Item label="Face Pulls" value="Face Pulls" />
+            <Picker.Item label="Bicep Curls" value="Bicep Curls" />
+            <Picker.Item label="Chin-Ups" value="Chin-Ups" />
+            <Picker.Item label="Tricep Push-Downs" value="Tricep Push-Downs" />
+            <Picker.Item label="Dips" value="Dips" />
+            <Picker.Item label="Wrist Curls" value="Wrist Curls" />
+            <Picker.Item label="Reverse Curls" value="Reverse Curls" />
+            <Picker.Item label="Crunches" value="Crunches" />
+            <Picker.Item label="Plank" value="Plank" />
+            <Picker.Item label="Leg Raises" value="Leg Raises" />
+            <Picker.Item label="Russian Twists" value="Russian Twists" />
+            <Picker.Item label="Side Plank" value="Side Plank" />
+            <Picker.Item label="Hyperextensions" value="Hyperextensions" />
+            <Picker.Item label="Deadlifts" value="Deadlifts" />
+            <Picker.Item label="Squats" value="Squats" />
+            <Picker.Item label="Lunges" value="Lunges" />
+            <Picker.Item label="Leg Extensions" value="Leg Extensions" />
+            <Picker.Item label="Hamstring Curls" value="Hamstring Curls" />
+            <Picker.Item label="Hip Thrusts" value="Hip Thrusts" />
+            <Picker.Item label="Glute Bridges" value="Glute Bridges" />
+            <Picker.Item label="Calf Raises" value="Calf Raises" />
+          </Picker>
+        </View>
 
-      <Button
-        onPress={recordWorkout}
-        title="New Workout"
-        color="#841584"
-      />
+        <TextInput
+          style={styles.inputBox }
+          placeholder="Number of Reps"
+          value={numReps}
+          onChangeText={setNumReps}
+          keyboardType="numeric"
+        />
 
-      <TouchableOpacity style={styles.button} onPress={handleSignOut}>
-        <Text style={styles.buttonText}>Sign Out</Text>
-      </TouchableOpacity>
-    </View>
+        <View style={styles.switchContainer}>
+          <Text 
+            style={styles.switchLabel}>Does this exercise include weights?
+          </Text>
+          <Switch
+            trackColor={{ true: blue }} //blue if switch is on
+            ios_backgroundColor="#909090" //gray if switch is off
+            value={usesWeight}
+            onValueChange={setUsesWeight}
+          />
+        </View>
+
+        {/* Conditionally render the numPounds input based on usesWeight */}
+        {usesWeight && (
+          <TextInput
+            style={styles.inputBox }
+            placeholder="Weight (in pounds)"
+            value={numPounds}
+            onChangeText={setNumPounds}
+            keyboardType="numeric"
+          />
+        )}
+
+        <TouchableOpacity
+          style={styles.newWorkoutButton}
+          onPress={recordWorkout}
+          >
+          <Text 
+            style={styles.buttonText}>Create New Workout
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          style={styles.button} 
+          onPress={handleSignOut}
+        >
+          <Text 
+          style={styles.buttonText}>Sign Out
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -89,24 +163,51 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   title: {
-    fontSize: 24,
+    fontSize: 30,
+    color: blue,
     fontWeight: 'bold',
     marginBottom: 20,
     textAlign: 'center',
+    textAlignVertical: 'top',
   },
   email: {
     textAlign: 'center',
     marginBottom: 20,
   },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center', // Align items vertically
+    marginBottom: 20, // Space below the switch
+  },
+  switchLabel: {
+    marginRight: 10, // Space between label and switch
+  },
   button: {
-    backgroundColor: '#007AFF',
+    backgroundColor: '#999',
     padding: 15,
     borderRadius: 5,
     marginBottom: 15,
+    marginHorizontal: 125,
+  },
+  newWorkoutButton: {
+    backgroundColor: blue, //blue
+    padding: 15,
+    borderRadius: 5,
+    marginBottom: 15,
+    marginHorizontal: 80,
+  },
+  inputBox: {
+    height: 50, // Increased height
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginBottom: 15, // space between inputs
+    paddingHorizontal: 15,
+    borderRadius: 5, // round the corners
   },
   buttonText: {
     color: '#fff',
     textAlign: 'center',
+    fontSize: 15,
     fontWeight: 'bold',
   },
 });
